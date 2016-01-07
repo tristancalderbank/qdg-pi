e4790ff62a7610765cb1374f4a541aac3a2ddb89
tristiano <tristan@oceansandlights.com>
2016-01-06 22:37:51 -0800

    subscribe stuff

--- redis/subscribe.py
+++ redis/subscribe.py
@@ -10,17 +10,26 @@ import string
 import csv
 
 
-file_writer = csv.writer(open("/home/tristiano/Desktop/DATA.csv", "wb"))
+# gets a valid message
 
-file_writer.writerow(["Time", "Temperature", "Pressure", "Humidity"])
+def get_message():
+    while True:
+        temp = pubsub.get_message()
+        if (temp is not None):
+            return temp
+
+
+# open file for writing
+data_file = open("/home/tristiano/Desktop/DATA.csv", "ab")
+file_writer = csv.writer(data_file)
 
 
 
 
-#connect to the Rasperry Pi Redis server
+# connect to the Rasperry Pi Redis server
 r = redis.StrictRedis(host='192.168.1.75', port=6379, db=0)
 
-#open up a pubsub instance
+# open up a pubsub instance
 pubsub = r.pubsub(ignore_subscribe_messages=True)
 
 
@@ -30,21 +39,29 @@ pubsub.subscribe('pressure')
 pubsub.subscribe('humidity')
 
 
+# main loop
+#-----------
+try:
 
-while True:
-	temp = pubsub.get_message()
-	if (temp is not None):
-		print temp['data']
+    while True:
 
+# make sure we get the messages in the right order
 
+        timestamp = get_message()
+        if (timestamp['channel'] == 'timestamp'):
+            timestamp = timestamp['data']
+            print timestamp
+            temperature = get_message()['data']
+            print temperature
+            pressure = get_message()['data']
+            print pressure
+            humidity = get_message()['data']
+            print humidity
 
+            file_writer.writerow([timestamp, temperature, pressure, humidity]) 
 
-
-
-
-
-
-
+except:
+    data_file.close()
 
 
 
