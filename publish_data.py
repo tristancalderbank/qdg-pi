dffba7575c53a29c30bf2d16066d26a6e5809899
tristan <tristan@oceansandlights.com>
2016-05-25 16:33:18 -0700

    Pi data publishing is done

--- publish_data.py
+++ publish_data.py
@@ -1,100 +1,105 @@
-#Basic imports
-import sys
-import math
-import random
-import os
-import time
+# QDG Pi Project
+# Quantum Degenerate Gases Lab (Madison Research Group)
+# publish_data.py
+# date: May 25, 2016
+# author: tristan calderbank
+# contact: tristan@alumni.ubc.ca
+# purpose: This python script first connects to the redis server,
+#          creates a 'pubsub' instance, subscribes to the appropriate
+#          channels, and begins reading the sensors connected to the Pi.
+#          It then publishes the data to the channels on the pubsub
+#          instance to be recieved by the data collection computer.
+
+from time import strftime
 import redis
-import string
 import smbus
-from datetime import datetime
-from pytz import timezone
-import pytz
-#Sensors
 from Adafruit_BME280 import *
 import RPi.GPIO as GPIO
 
+# configuration
+room_name = "master_table"
+server_ip = '10.1.137.176'
+server_port = 6379
+max_number_of_sensors = 8
+digits_to_round_to = 2
 
-ip = '10.1.137.162'
-
-#connect to the Redis server
-r = redis.StrictRedis(host=ip, port=6379, db=0)
-
-#open up a pubsub instance
-pubsub = r.pubsub(ignore_subscribe_messages=True)
-
-pubsub.subscribe('timestamp')
-pubsub.subscribe('temperature')
-pubsub.subscribe('pressure')
-pubsub.subscribe('humidity')
-
-debug_time = 0
-
-
-
-# Multiplexer stuff
-
-I2C_address = 0x76
+# multiplexer 
+I2C_address = 0x70
 I2C_bus_number = 1
-I2C_ch_0 = 0b00000001
-I2C_ch_1 = 0b00000010
-I2C_ch_2 = 0b00000100
-I2C_ch_3 = 0b00001000
-I2C_ch_4 = 0b00010000
-I2C_ch_5 = 0b00100000
-I2C_ch_6 = 0b01000000
-I2C_ch_7 = 0b10000000
+I2C_ch_4 = 0b00000001
+I2C_ch_5 = 0b00000010
+I2C_ch_6 = 0b00000100
+I2C_ch_7 = 0b00001000
+I2C_ch_3 = 0b00010000
+I2C_ch_2 = 0b00100000
+I2C_ch_1 = 0b01000000
+I2C_ch_0 = 0b10000000
 
-def I2C_setup(i2c_channel_setup):
+mux_channel = [I2C_ch_0, I2C_ch_1, I2C_ch_2, I2C_ch_3, I2C_ch_4, I2C_ch_5, I2C_ch_6, I2C_ch_7]
+
+def set_mux_channel(i2c_channel_setup):
     bus = smbus.SMBus(I2C_bus_number)
     bus.write_byte(I2C_address,i2c_channel_setup)
     time.sleep(0.1)
-    #print "TCA9548A I2C channel status:", bin(bus.read_byte(I2C_address))
+#   print "TCA9548A I2C channel status:", bin(bus.read_byte(I2C_address))
 
+def read_sensors(number_of_sensors):
+	sensor_data = []
+	for channel in range(0,number_of_sensors):
 
-channel = 1;
+		try:
+			set_mux_channel(mux_channel[channel])
+			sensor = BME280(mode=BME280_OSAMPLE_8)
+			temperature = round(sensor.read_temperature(), digits_to_round_to)
+			pressure = round(sensor.read_pressure(), digits_to_round_to)
+			humidity = round(sensor.read_humidity(), digits_to_round_to)
+			sensor_data.append((temperature,pressure,humidity))
 
-while True:
-
-	if(channel == 1):
-		channel = 0
-	else:
-		channel = 1	
-
-	if(channel == 1):
-		I2C_setup(I2C_ch_1)
-	else:
-		I2C_setup(I2C_ch_0)
-
-	sensor = BME280(mode=BME280_OSAMPLE_8)
-        dt = time.time()
-
-        
-        temperature = round(sensor.read_temperature(), 2)
-        pressure = round(sensor.read_pressure(), 2)
-        humidity = round(sensor.read_humidity(), 2)
-
-        r.publish('timestamp', dt)
-        r.publish('temperature', temperature) 
-        r.publish('pressure', pressure) 
-        r.publish('humidity', humidity) 
-
-	if(channel == 1):
-		print 'reading sensor 1...'#	else:
-	else:
-		print 'reading sensor 0...'
-
-        print pubsub.get_message()
-        print pubsub.get_message()
-        print pubsub.get_message()
-        print pubsub.get_message()
-
-	debug_time+=1
-	print debug_time
-
-
-        time.sleep(2)
+		except IOError: 
+			print "Failed to read sensor channel " + str(channel)
+			sensor_data.append("no_data")
 	
+	return sensor_data
 
+
+def connect_to_server(server_ip,server_port):
+	r = redis.StrictRedis(host=server_ip, port=server_port, db=0)
+	connected = False
+	while connected == False:
+		try:
+			server_connection = redis.StrictRedis(host=server_ip, port=server_port, db=0)
+			pubsub = server_connection.pubsub(ignore_subscribe_messages=True)
+			pubsub.subscribe(room_name)
+			connected = True
+			print "Successfully connected to redis-server at " + server_ip +"."
+		except redis.exceptions.ConnectionError:
+			print "Couldn't connect to redis server at " + server_ip + ", trying again in 5 seconds..."
+			time.sleep(5)
+
+	return server_connection
+
+def check_if_connected(server_connection, server_ip, server_port):
+	try:
+		server_connection.ping()
+		print "Still connected to server at " + server_ip + "."	
+	except redis.exceptions.ConnectionError:
+		print "Server at " + server_ip + " was disconnected, trying again..."
+		server_connection = connect_to_server(server_ip, server_port)
+
+	return server_connection
+
+
+# connect to the Redis server
+server_connection = connect_to_server(server_ip, server_port)
+
+# main loop
+while True:
+	sensor_data = read_sensors(max_number_of_sensors)
+	timestamp = strftime("%Y-%m-%d %H:%M:%S")
+	sensor_data.insert(0, timestamp)
+
+	server_connection = check_if_connected(server_connection, server_ip, server_port)	
+	server_connection.publish('master_table', sensor_data)
+	time.sleep(1)
 
 
